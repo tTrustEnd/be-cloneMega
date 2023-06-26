e8d584254a8c614d0b980f41c66d69e12f3477e8
Truong <banoianh1x@gmail.com>
2023-06-26 14:47:47 +0700

    add vnpay

--- src/server.js
+++ src/server.js
@@ -12,7 +12,10 @@ const configStatic = require('./config/static');
 const routerUser = require('./routes/user');
 const routerFilms = require('./routes/films');
 const routerUudai = require('./routes/uudai');
-
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var order = require('./routes/order');
 
 // const mongoose = require('mongoose');
 // mongoose.connect('mongodb+srv://root:5ALUF5no5iqaP9dM@cluster0.gwlf8ij.mongodb.net/?retryWrites=true&w=majority')
@@ -41,6 +44,40 @@ app.use('/v1/api', routerUser);
 app.use('/v2/api', routerFilms);
 app.use('/v3/api', routerUudai);
 
+// VNP
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/order', order);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.listen
+module.exports = app;
+//
 
 
 (async () => {
