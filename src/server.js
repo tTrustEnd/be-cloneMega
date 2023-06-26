45e1bc514d207d2d8d21c01717939dd27b0d110e
Truong <banoianh1x@gmail.com>
2023-06-26 10:41:27 +0700

    ss

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 // Cấu hình CORS middleware
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Hoặc thay '*' bằng origin cụ thể nếu bạn chỉ muốn cho phép từ một số nguồn cụ thể.
+  res.setHeader('Access-Control-Allow-Origin', corsOptions); // Hoặc thay '*' bằng origin cụ thể nếu bạn chỉ muốn cho phép từ một số nguồn cụ thể.
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
